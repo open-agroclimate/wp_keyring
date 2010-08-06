ce32fc122f58adce0fb104394ca7ced9acae0066
Christopher Villalobos <frostbytten@design.abe.ufl.edu>
2010-08-06 18:04:39 -0400

    Initial Import
    * Created the required WordPress header information for the plugin.
    * Ready to start hacking from here

---
+++ keyring.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: wp_keyring
+Plugin URI:  http://git.agroclimate.org/oactools/wp_keyring
+Description: Coming soon
+Version: 1.0
+Author: The Open AgroClimate Team
+Author URI: http://open.agroclimate.org/
+License: BSD Modified
+*/
+?>
