16c9689fcd129207caca4e6e7e2f24931f6b99ca
Christopher Villalobos <cvillalobos@ufl.edu>
2010-08-11 11:56:25 -0400

    Created Menu Item
    * Created the WP Keyring menu item under "Settings"
    * Started basic look-n-feel for the form.

--- keyring.php
+++ keyring.php
@@ -9,5 +9,19 @@ Author URI: http://open.agroclimate.org/
 License: BSD Modified
 */
 
-echo "Something is coming soon. I promise"
+add_action('admin_menu','wp_keyring_menu');
+
+function wp_keyring_menu() {
+	add_management_page('WP Keyring Settings', 'WP Keyring', 'manage_options', 'wp_keyring_settings', 'wp_keyring_settings');
+}
+
+function wp_keyring_settings() {
+	if(!current_user_can('manage_options')) {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+	
+	echo '<div class="wrap">';
+	echo '<p>The Form Goes Here</p>';
+	echo '</div>';
+}
 ?>
