264fe551bbc327cc5d7e389049a900cc66bb7ac9
Christopher Villalobos <cvillalobos@ufl.edu>
2010-08-11 17:15:41 -0400

    Installer correctly installing and upgrading database.
    * Removed wp_kill() code for DEBUG
    * Cleaned up formatting to be consistent with WordPress Coding Standards
    * Created upgrading capabilities and verified functionality.

--- keyring.php
+++ keyring.php
@@ -22,21 +22,30 @@ function wp_keyring_installer() {
 	global $wp_keyring_db_version;
 	global $wpdb;
 	
-	$table_name = $wpdb->prefix . 'keyring_plugin';
+	// Only run this if you don't have the correct version (or any version at all).
+	// NOTE: I know you could short circuit this by creating it all manually, but you can mess up your own install if you want.	
+	if ( ( !get_option( "wp_keyring_db_version") ) || ( get_option( "wp_keyring_db_version" ) != $wp_keyring_db_version) ) {
+		$table_name = $wpdb->prefix . 'keyring_plugin';
+		$structure = "CREATE TABLE $table_name (
+			id INT(9) NOT NULL AUTO_INCREMENT,
+			blog_id INT(11) NOT NULL DEFAULT '0',
+			key_id VARCHAR(25) NOT NULL,
+			name VARCHAR(100) NOT NULL DEFAULT '',
+			api_key TEXT NOT NULL DEFAULT '',
+			PRIMARY KEY  (id),
+			KEY  (key_id)
+			);";
 	
-	$structure = "CREATE TABLE $table (
-		id INT(9) NOT NULL AUTO_INCREMENT,
-		blog_id INT(11) NOT NULL DEFAULT '0',
-		key_id VARCHAR(25) NOT NULL,
-		name VARCHAR(100) NOT NULL DEFAULT '',
-		api_key TEXT NOT NULL DEFAULT '',
-		PRIMARY KEY  (id),
-		KEY  (key_id)
-		);";
-		
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
-		$result = dbDelta( $structure );
-		wp_die( var_dump( $result ));
+	
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $structure );
+	}
+	
+	// If this is the first time running the structure, create the option. If you are upgrading, update the version as well.
+	if (!get_option( "wp_keyring_db_version" ) )	
+		add_option( "wp_keyring_db_version", $wp_keyring_db_version );
+	if ( get_option( "wp_keyring_db_version" ) != $wp_keyring_db_version )
+		update_option( "wp_keyring_db_version", $wp_keyring_db_version );
 }
 
 function wp_keyring_menu() {
