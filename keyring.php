1a212887dd0add10abdbd940278219dc3aa36e4c
Christopher Villalobos <cvillalobos@ufl.edu>
2010-08-11 16:35:34 -0400

    Deliberately crashing the installer to check the database.
    dbDelta is crashing for some unknown reason and (WP_DEBUG, true) isn't doing
    anything for me so I have to try something else.

--- keyring.php
+++ keyring.php
@@ -9,7 +9,35 @@ Author URI: http://open.agroclimate.org/
 License: BSD Modified
 */
 
+// GLOBAL VARIABLES
+global $wp_keyring_db_version;
+$wp_keyring_db_version = "1.0";
+
+// HOOKS AND ACTIONS
 add_action( 'admin_menu', 'wp_keyring_menu' );
+register_activation_hook( __FILE__, 'wp_keyring_installer');
+
+
+function wp_keyring_installer() {
+	global $wp_keyring_db_version;
+	global $wpdb;
+	
+	$table_name = $wpdb->prefix . 'keyring_plugin';
+	
+	$structure = "CREATE TABLE $table (
+		id INT(9) NOT NULL AUTO_INCREMENT,
+		blog_id INT(11) NOT NULL DEFAULT '0',
+		key_id VARCHAR(25) NOT NULL,
+		name VARCHAR(100) NOT NULL DEFAULT '',
+		api_key TEXT NOT NULL DEFAULT '',
+		PRIMARY KEY  (id),
+		KEY  (key_id)
+		);";
+		
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
+		$result = dbDelta( $structure );
+		wp_die( var_dump( $result ));
+}
 
 function wp_keyring_menu() {
 	add_management_page( 'WP Keyring Settings', 'WP Keyring', 'manage_options', 'wp_keyring_settings', 'wp_keyring_settings' );
